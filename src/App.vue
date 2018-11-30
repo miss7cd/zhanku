04fda9bd1aa7cbce26dcc0a492ae751c83b9c7c6
wangmin <1399149766@qq.com>
2018-11-30 17:29:14 +0800

    创建page-header组建

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <page-header></page-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import pageHeader from '@/components/page-header/page-header'
 export default {
-  name: 'App'
+  name: 'App',
+  conponents: {
+    pageHeader
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/less/base.less';
 </style>
---
+++ src/components/page-header/page-header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="page-header">
+    <div class="logo"></div>
+    <nav>
+      <ul>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+        <li>
+          <a href>首页</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+@import "./../../assets/less/mixin.less";
+.page-header {
+  width: 100%;
+  height: 50px;
+  background-color: @theme_color;
+}
+</style>
